6b597f3e51b6809caf6c43f79db4f0a2f99787b8
Ríad Oliveira <riad.oliveira@hotmail.com>
2021-01-13 00:48:01 -0300

    .env added to gitignore

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -6,7 +6,8 @@ const SignUp: React.FC = () => (
         <LogoView>
             <LogoImage
                 source={{
-                    uri: '',
+                    uri:
+                        'https://s3-sa-east-1.amazonaws.com/projetos-artes/fullsize%2F2017%2F08%2F22%2F23%2FLogo-164750_454671_231322223_2120246239.jpg',
                 }}
             />
         </LogoView>
